a6a37b51a71630888fe5a8647ca7a494eda7d068
PlsDeleteMyLife <cdranton@gmail.com>
2019-03-03 17:44:15 +0700

    Begining of bignum calc.

---
+++ calc.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+int main(void)
+{
+	/* Variable  */
+	unsigned long long int num_size1, num_size2;
+	std::string s1, s2;
+	char action;
+	/* Inputing  */
+	std::cout << "Enter the first number: ";
+	std::cin >> s1;
+	std::cout << "\nEnter the second number: ";
+	std::cin >> s2;
+	std::cout << "\n(+ - * / )\nEnter the operation for action: ";
+	std::cin >> action;
+	std::cout << std::endl;
+	/* End of inputing  */
+	//
+	return 0;
+}
