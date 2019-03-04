5034b315fdb3b392df57fb2833329d6195adbb70
PlsDeleteMyLife <cdranton@gmail.com>
2019-03-04 19:11:53 +0700

    Creating the massives and starter to action w/ num's

--- calc.cpp
+++ calc.cpp
@@ -7,6 +7,7 @@ int main(void)
 	/* Variable  */
 	unsigned long long int num_size1, num_size2;
 	std::string s1, s2;
+	int pause;
 	char action;
 	/* Inputing  */
 	std::cout << "Enter the first number: ";
@@ -15,8 +16,31 @@ int main(void)
 	std::cin >> s2;
 	std::cout << "\n(+ - * / )\nEnter the operation for action: ";
 	std::cin >> action;
+	/*          */
+	std::cout << "\nSize of first number - "  << s1
+			  << "\nSize of second number - " << s2 << std::endl;
+	/* Array creating */
+	short int Array1[s1.size()];
+	short int Array2[s2.size()];
+	for(auto i = 0; i < s1.size(); i++)
+		Array1[i] = s1[i] - 0x30;
+	for(auto i = 0; i < s2.size() ; i++)
+		Array2[i] = s2.at(i);
+	/* 		          */
 	std::cout << std::endl;
-	/* End of inputing  */
-	//
+	switch(action)
+	{
+		case('+'):
+		{
+			//
+			break;
+		}
+		default:
+		{
+			//
+		}
+	}
+	//for(auto)
+	//std::cin >> pause; 
 	return 0;
 }
