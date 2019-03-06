7e3f7add62a897aa14d409e6275bb7e384a44a59
PlsDeleteMyLife <cdranton@gmail.com>
2019-03-07 01:52:04 +0700

    Nums are set. Sum scoring on process

--- source/calc.cpp
+++ source/calc.cpp
@@ -26,7 +26,6 @@ int main(void)
 	int Array1[max];
 	int Array2[max];
 	int Array_sum[max];
-	std::stringstream ss(s2);
 	for(auto i = 0; i < max; i++)
 	{
 		Array1[i] = 0;
@@ -35,33 +34,13 @@ int main(void)
 	}
 	for(auto i = max - s1.size(); i < max; i++)
 	{
-	
-		if(s1[temp] == '1')
-			Array1[i] = 1;
-		else if(s1[temp] == '2')
-			Array1[i] = 2;
-		else if(s1[temp] == '3')
-			Array1[i] = 3;
-		else if(s1[temp] == '4')
-			Array1[i] = 4;
-		else if(s1[temp] == '5')
-			Array1[i] = 5;
-		else if(s1[temp] == '6')
-			Array1[i] = 6;
-		else if(s1[temp] == '7')
-			Array1[i] = 7;
-		else if(s1[temp] == '8')	
-			Array1[i] = 8;
-		else if(s1[temp] == '9')
-			Array1[i] = 9;
-			temp++;
+		Array1[i] = s1[temp] - 0x30;
+		temp++;
 	}
 	temp = 0;
 	for(auto i = max - s2.size(); i < max; i++)
 	{
-//		std::string swap = s1[i];
-//		Array2[i] = std::stoi(swap);
-		Array2[i] = s1[i] - 0x30;
+		Array2[i] = s2[temp] - 0x30;
 		temp++;
 	}
 	std::cout << "\n";
@@ -71,19 +50,21 @@ int main(void)
 	for(auto i = 0; i < max; i++)
 		std::cout << Array2[i];
 	std::cout << "\n";
+	temp = 0;
 	switch(action)
 	{
 		case('+'):
-		{
-//DOBIT' NULYAMI I VSE NICE		
+		{		
 			for(auto i = max; i < 0; i--)
 			{
-				Array_sum[i] = Array1[i] + Array2[i];
+				Array_sum[i] = Array1[i] + Array2[i] + temp;
 				if(Array_sum[i] > 9)
 				{
 					Array_sum[i] -= 10;
-					Array1[i - 1]++;
+					temp = 1;
 				}
+				else
+					temp = 0;
 			}
 				for(auto i = 0; i < max; i++)
 					std::cout << Array_sum[i];
@@ -95,8 +76,5 @@ int main(void)
 			//
 		}
 	}
-	//for(auto)
-	//std::cout << std::endl;
-	//std::cin >> pause;
 	return 0;
 }
