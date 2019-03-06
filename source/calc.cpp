3bf84aaa290c671fb80b8c9f548392d46b7b8574
PlsDeleteMyLife <cdranton@gmail.com>
2019-03-07 00:23:05 +0700

    Look

--- source/calc.cpp
+++ source/calc.cpp
@@ -23,80 +23,80 @@ int main(void)
 		max += s2.size();
 		min = s1.size();
 	}
-	unsigned short int Array1[max];
-	unsigned short int Array2[max];
-	unsigned short int Array_sum[max];
+	int Array1[max];
+	int Array2[max];
+	int Array_sum[max];
 	std::stringstream ss(s2);
-//	for(auto i = 0; i < max; i++)
-//	{
-//		Array1[i] = 0;
-//		Array2[i] = 0;
-//	}
-	for(auto i = max - s1.size(); i < max;)
+	for(auto i = 0; i < max; i++)
 	{
-		if(s1[i] == '1')
-			Array1[i] = 1;
-		else if(s1[i] == '2')
-			Array1[i] = 2;
-		else if(s1[i] == '3')
-			Array1[i] = 3;
-		i++;
+		Array1[i] = 0;
+		Array2[i] = 0;
+		Array_sum[i] = 0;
 	}
-//	ss[s2.size()] >> Array2[max];
-//	Array1[i] = s1[i] - 0x30;
-//	for(auto i = max; i < max - s2.size(); i--)
-//		Array2[i] = s1[i] - 48;
-//	for(auto i = max - s2.size(); i < 0; i--)
-//		Array2[i] = '0' - 48;
+	for(auto i = max - s1.size(); i < max; i++)
+	{
+	
+		if(s1[temp] == '1')
+			Array1[i] = 1;
+		else if(s1[temp] == '2')
+			Array1[i] = 2;
+		else if(s1[temp] == '3')
+			Array1[i] = 3;
+		else if(s1[temp] == '4')
+			Array1[i] = 4;
+		else if(s1[temp] == '5')
+			Array1[i] = 5;
+		else if(s1[temp] == '6')
+			Array1[i] = 6;
+		else if(s1[temp] == '7')
+			Array1[i] = 7;
+		else if(s1[temp] == '8')	
+			Array1[i] = 8;
+		else if(s1[temp] == '9')
+			Array1[i] = 9;
+			temp++;
+	}
+	temp = 0;
+	for(auto i = max - s2.size(); i < max; i++)
+	{
+//		std::string swap = s1[i];
+//		Array2[i] = std::stoi(swap);
+		Array2[i] = s1[i] - 0x30;
+		temp++;
+	}
+	std::cout << "\n";
 	for(auto i = 0; i < max; i++)
 		std::cout << Array1[i];
 		std::cout << std::endl;
 	for(auto i = 0; i < max; i++)
 		std::cout << Array2[i];
-/*
-//	switch(action)
-//	{
-//		case('+'):
-//		{
+	std::cout << "\n";
+	switch(action)
+	{
+		case('+'):
+		{
 //DOBIT' NULYAMI I VSE NICE		
 			for(auto i = max; i < 0; i--)
 			{
-				if(Array1[i] == NULL)
-					Array_sum[i] = Array2[i];
-				else if(Array2[i] == NULL)
-					Array_sum[i] = Array1[i];
-				else
-					Array_sum[i] = Array1[i] + Array2[i];
+				Array_sum[i] = Array1[i] + Array2[i];
 				if(Array_sum[i] > 9)
 				{
-					Array_sum[i] = Array_sum[i] % 10;
+					Array_sum[i] -= 10;
 					Array1[i - 1]++;
 				}
 			}
-				std::cout << "  ";
-				for(auto i = 0; i < s1.size(); i++)
-					std::cout << Array1[i];
-				std::cout << "\n+ ";
-				for(auto i = 0; i < s2.size(); i++)
-					std::cout << Array2[i];
-				std::cout << "\n  ";
-				for(auto i = 0; i < max; i++)
-					std::cout << "_";
-				std::cout << std::endl;
 				for(auto i = 0; i < max; i++)
 					std::cout << Array_sum[i];
 				std::cout << std::endl;
-*/
-//			}
-//			break;
-//		}
-//		default:
-//		{
+		}
+		break;
+		default:
+		{
 			//
-//		}
-//	}
+		}
+	}
 	//for(auto)
-	std::cout << std::endl;
+	//std::cout << std::endl;
 	//std::cin >> pause;
 	return 0;
 }
